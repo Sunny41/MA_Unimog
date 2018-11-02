d739524da16b7b0b838c1580d76103174e7b3294
SteCarl <stefano.carluccio@student.reutlingen-university.de>
2018-11-02 15:13:26 +0100

    Tutorial-level, Touch-Input and Camera started

---
+++ MA_Unimog/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public Joystick joystick;
+
+    public float driveSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+
+    void Update()
+    {
+
+        //horizontalMove = Input.GetAxisRaw("Horizontal") * driveSpeed;
+        horizontalMove = joystick.Horizontal * driveSpeed;
+
+        //Debug.Log(horizontalMove);
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
