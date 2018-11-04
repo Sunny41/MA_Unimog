2685541959d0a57d93b62e0fab02e3541ca667cb
Cthulhu <jannik.renner@Student.reutlingen-university.de>
2018-11-04 22:21:41 +0100

    added main menu. added unimog json to create unimogs with stats to load into game

---
+++ MA_Unimog/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject[] panels;
+
+    private void Start()
+    {
+        InitializeMainMenu();   
+    }
+    
+
+    private void InitializeMainMenu()
+    {
+        foreach(GameObject go in panels)
+        {
+            go.SetActive(false);
+            if(go.name == "MainMenu")
+            {
+                go.SetActive(true);
+            }
+        }
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
---
+++ MA_Unimog/Assets/Scripts/UI/UnimogSelectMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using LitJson;
+
+public class UnimogSelectMenu : MonoBehaviour {
+    
+    private JsonData unimogData;
+
+    void Start()
+    {
+        unimogData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Scripts/JSON/unimogs.json"));
+        ConstructItemDatabase();
+    }
+
+    void ConstructItemDatabase()
+    {
+        for (int i = 0; i < unimogData.Count; i++)
+        {
+            Debug.Log((string)unimogData[i]["name"]);
+        }
+    }
+
+}
