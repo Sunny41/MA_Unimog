72732896a51aabe7e2476747b0f32ec624830477
Tim <Tim>
2018-12-04 18:41:21 +0100

    Level beendet Nachricht eingebaut

---
+++ MA_Unimog/Assets/Win.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Win : MonoBehaviour {
+    public Text winscreen;
+
+	// Use this for initialization
+	void Start () {
+        winscreen.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        winscreen.text = "Level geschaft \n Kisten: X von X \n Zeit: X \n Punkte: X ";
+    }
+}
