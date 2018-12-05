3f3c58de9cfb2643f517e23e8788e977881f230b
Tim <Tim>
2018-12-05 12:22:48 +0100

    Zeitmessung eingebaut und für Punkte etwas vorbereitet

--- MA_Unimog/Assets/Win.cs
+++ MA_Unimog/Assets/Win.cs
@@ -5,18 +5,38 @@ using UnityEngine.UI;
 
 public class Win : MonoBehaviour {
     public Text winscreen;
+    public int maxBox;
+    public static float time;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         winscreen.text = "";
-	}
+        maxBox = getMaxBoxes(); //later deplace withe function to get max 
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        
+
+    }
     void OnCollisionEnter2D(Collision2D coll)
     {
-        winscreen.text = "Level geschaft \n Kisten: X von X \n Zeit: X \n Punkte: X ";
+        int points = calculatePoints();
+        winscreen.text = "Level geschaft \n Kisten: X von "+ maxBox +"  \n Zeit: "+ Time.fixedTime + "  \n Punkte: "+ points ;
+    }
+
+    //later calculate Points
+    private int calculatePoints()
+    {
+
+        return 3;
+    }
+    private int getMaxBoxes()
+    {
+
+        return 3;
     }
 }
