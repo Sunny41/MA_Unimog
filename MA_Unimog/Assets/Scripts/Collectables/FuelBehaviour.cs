641c41ffddd79ac42473dbab7925a29c09f518d1
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2018-12-08 14:52:56 +0100

    Fuel logik beim Sammeln angefangen

---
+++ MA_Unimog/Assets/Scripts/Collectables/FuelBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelBehaviour : MonoBehaviour {
+
+    public float fuelValue = 0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")){
+            Debug.Log("FUEL COLLECTED"); //add fuelValue to fuelStatus
+
+            Destroy(this.gameObject);
+        }
+    }
+}
