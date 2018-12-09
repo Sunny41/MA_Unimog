2fe33e1347f8f0167d8ce946971befad4ee71d9d
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2018-12-09 13:12:28 +0100

    tilt funktion eingefügt
     -> muss ziemlich sicher überarbeitet werden wenn man das später besser testen kann

--- MA_Unimog/Assets/Scripts/PlayerMovement.cs
+++ MA_Unimog/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,6 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
 
     public Joystick driveJoystick;
-    //public Joystick tiltJoystick;
 
     public Animator frontWheelAnimator;
     public Animator backWheelAnimator;
@@ -36,15 +35,6 @@ public class PlayerMovement : MonoBehaviour
             backWheelAnimator.SetTrigger("driving");
         }
 
-        //tilt = tiltJoystick.Horizontal * tiltAmount;
-
-        if(tilt > 0){
-            Debug.Log(tilt);
-        }
-
-
-
-
     }
 
     void FixedUpdate()
@@ -53,5 +43,15 @@ public class PlayerMovement : MonoBehaviour
         controller.transform.localScale = new Vector3(1, 1, 1);
 
         controller.Tilt(tilt * Time.fixedDeltaTime);
+    }
+
+    public void tiltForwards()
+    {
+        this.tilt += tiltAmount;
+    }
+
+    public void tiltBackwards()
+    {
+        this.tilt -= tiltAmount;
     }
 }
