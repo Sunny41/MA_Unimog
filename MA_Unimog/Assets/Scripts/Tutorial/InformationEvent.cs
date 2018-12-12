f7d6c7f640e5dd1660f3763f9098d077e4e81feb
Cthulhu <jannik.renner@Student.reutlingen-university.de>
2018-12-12 14:38:43 +0100

    startet with tutorial level

---
+++ MA_Unimog/Assets/Scripts/Tutorial/InformationEvent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class InformationEvent : MonoBehaviour {
+
+    [SerializeField] private GameObject go;
+    private bool started;
+
+    void Awake()
+    {
+        started = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            ShowInformation();
+        }
+    }
+
+    private void ShowInformation()
+    {
+        if (!started)
+        {
+            started = true;
+            go.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void Done()
+    {
+        go.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
