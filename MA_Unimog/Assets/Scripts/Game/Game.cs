946f577eb6c47d7ceff39e6de300238e51699fc1
Cthulhu <jannik.renner@Student.reutlingen-university.de>
2018-12-20 13:26:00 +0100

    started ingame logic. victory screen prefab

---
+++ MA_Unimog/Assets/Scripts/Game/Game.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform spawn;
+    [SerializeField] private VictoryScreen victoryScreen;
+    //[SerializedField] private GameOverScreen gameOverScreen;
+
+	public void Start () {
+        //Set Player to spawn point
+        Vector2 position = new Vector2(spawn.position.x, spawn.position.y + 0.5f);
+        player.transform.position = position;
+
+        GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        //Load level informations
+        string sceneId = gm.GetSceneId();
+                
+	}
+	
+	void Update () {
+		
+	}
+}
