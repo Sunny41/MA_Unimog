f5170989e18652c221a756d6c2a5561d60f05371
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2018-12-20 20:59:34 +0100

    Steuerung Komplett erneuert/verbessert + lvl2 angefangen
    Getter für Kisten auf Unimog hinzugefügt unter cargoCheck

---
+++ MA_Unimog/Assets/Scripts/CarAttributes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAttributes : MonoBehaviour {
+
+    public Transform cargoCheck;
+    public LayerMask whatIsBox;
+    public float fuel = 500f;
+
+    private int boxes = 0;
+
+
+    public float GetFuelStatus(){
+        return this.fuel;
+    }
+
+    public void AddFuel(float fuelAmount)
+    {
+        this.fuel += fuelAmount;
+    }
+
+    public int GetBoxesAmount()
+    {
+        return this.boxes;
+    }
+
+    private void FixedUpdate()
+    {
+
+        //Debug.Log(GameObject.FindGameObjectsWithTag("boxes"));
+        GameObject[] boxesOnCar = GameObject.FindGameObjectsWithTag("boxes");
+
+        Collider2D collider = cargoCheck.GetComponentInParent<Collider2D>();
+
+        int boxesTemp = 0;
+        for (int i = 0; i < boxesOnCar.Length; i++)
+        {
+            if (collider.IsTouching(boxesOnCar[i].GetComponent<Collider2D>()))
+            {
+                boxesTemp++;
+            }
+        }
+
+        this.boxes = boxesTemp;
+        Debug.Log(GetBoxesAmount());
+    }
+}
